deaa9d939a82e41864f8078c3d48711b1e93c1ad
Ehmkaay <mk.agbola@gmail.com>
2023-12-14 08:46:14 -0800

    6-list_values.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- e a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.;
-CALL GetTableStructure('hbtn_0c_0', 'first_table');
+SHOW CREATE TABLE first_table;
