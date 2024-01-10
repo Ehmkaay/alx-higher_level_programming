6ab573196621304de512339ac7fcf2fdef4ab1f0
Ehmkaay <mk.agbola@gmail.com>
2024-01-10 08:27:42 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 0) {
-	console.log('No argument');
+    console.log('No argument');
 } else if (process.argv.length === 1) {
-	console.log('Argument found');
+    console.log('Argument found');
 } else {
-	console.log('Argument found');
+    console.log('Argument found');
 }
