e482577cf256e7bba246c5d1299911451b12eb14
Ehmkaay <mk.agbola@gmail.com>
2024-01-10 09:01:08 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-
-if (args.length === 2) {
+if (process.argv.length === 2) {
     console.log('No argument');
-} else if (args.length === 3) {
+} else if (process.argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('Argument found');
